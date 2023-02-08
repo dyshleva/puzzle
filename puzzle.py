c3f086dc5b0c9f1df197f0411e2e09429212f1ec
dyshleva <116552489+dyshleva@users.noreply.github.com>
2023-02-08 23:45:57 +0200

    block check

--- puzzle.py
+++ puzzle.py
@@ -56,8 +56,49 @@ def column_check(board: list) -> bool:
         lst = []
     return True
 
-def block_check(board):
-    pass
+def block_check(board:list) -> bool:
+    """
+    Check for same digits in color block
+    Args:
+        board (list): playboard
+    Returns:
+        bool: True if no same digits in every color block, else False
+    >>> block_check([\
+ "**** ****",\
+ "***1 ****",\
+ "**  3****",\
+ "* 4 1****",\
+ "     9 5 ",\
+ " 6  83  *",\
+ "3   1  **",\
+ "  8  2***",\
+ "  2  ****"])
+    True
+    >>> block_check([\
+ "**** ****",\
+ "***1 ****",\
+ "**  3****",\
+ "* 4 1****",\
+ "     9 5 ",\
+ " 6  83  *",\
+ "3   1  **",\
+ "  8  2***",\
+ "  22 ****"])
+    False
+    """
+    i = 1
+    while i <= 5:
+        numbers = ""
+        numbers += board[-i][i-1:i+4]
+        j = i
+        while j <= i+4:
+            numbers += board[-j][i-1]
+            j += 1
+        if len(set(numbers.replace(" ",""))) != len(numbers.replace(" ","")) or "0" in numbers:
+            return False
+        i+=1
+    return True
+
 
 def final_check(board):
     pass
