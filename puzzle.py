cc7268fe6016724ec0cd32b60217db98b984cebb
Baredal <116868315+Baredal@users.noreply.github.com>
2023-02-08 11:19:12 +0200

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -3,9 +3,9 @@ def line_check(board):
     Check for same digits in row
     :param (board) : input board
     :param (return) : True if no same digits else False
-    >>> rows_checkers(["**** ****", "***1 ****", "**  3****", "* 4 1****", "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    >>> line_check(["**** ****", "***1 ****", "**  3****", "* 4 1****", "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
     True
-    >>> rows_checkers(["**** ****", "***1 ****", "**  321**", "* 4 111**", "     9 5 ", " 67383  *", "3   1  **", "  8  2***", "  2  ****"])
+    >>> line_check(["**** ****", "***1 ****", "**  321**", "* 4 111**", "     9 5 ", " 67383  *", "3   1  **", "  8  2***", "  2  ****"])
     False
     '''
     board = [row.replace('*', '').replace(' ', '') for row in board] 
