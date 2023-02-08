baedf64c83ed2352241886f7c215da19e4236336
dyshleva <116552489+dyshleva@users.noreply.github.com>
2023-02-08 11:04:16 +0200

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -7,6 +7,34 @@ def column_check(board):
 def block_check(board):
     pass
 
-def final_check(column, ryadok, block):
-    pass
+def final_check(board):
+    '''
+    >>> final_check([
+ "**** ****",
+ "***1 ****",
+ "**  3****",
+ "* 4 1****",
+ "     9 5 ",
+ " 6  83  *",
+ "3   1  **",
+ "  8  2***",
+ "  2  ****"
+])
+    False
+    >>> final_check([
+ "**** ****",
+ "***1 ****",
+ "**  3****",
+ "* 4 1****",
+ "     9 5 ",
+ " 6  83  *",
+ "3   7  **",
+ "  8  2***",
+ "  2  ****"
+])
+    True
+    '''
+    if line_check(board) and column_check(board) and block_check(board):
+        return True
+
 
